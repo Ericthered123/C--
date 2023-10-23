2c6915f82c37b18a14364701a0ce9ec8d0049195
Ericthered123 <eric6doyle@gmail.com>
2023-10-23 09:15:47 -0300

    Extermination of numbers

--- .vscode/ej_8.cpp
+++ .vscode/ej_8.cpp
@@ -91,7 +91,7 @@ nodo_int* load_of_ints(nodo_int* inicio)
         deseo=verify_wish();
         if (deseo==false)
         {
-            cout <<endl<< "FINALIZANDO...";
+            cout <<endl<< "FINALIZANDO..."<<endl;
             std::this_thread::sleep_for(std::chrono::seconds(3));
             break;                   
         }
@@ -103,12 +103,83 @@ nodo_int* load_of_ints(nodo_int* inicio)
     }
     return inicio;
 }
+bool verify_extermination(nodo_int* inicio,int exterminated_number)
+{
+    for (nodo_int *aux = inicio; aux!=nullptr; aux=aux->siguiente)
+    {
+        if (aux->num==exterminated_number)
+        {
+            
+            return true;
+        }
+                
+    }
+    cout << "El numero ingresado ha sido en la lista...";
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    return false;
+}
+nodo_int* exterminate_number(nodo_int* inicio,int chosen_number)
+{ 
+    while (verify_extermination(inicio,chosen_number))
+    {
+        
+    
+    
+        if (inicio!=nullptr)
+        {
+            nodo_int* aux=inicio;
+            if (chosen_number==inicio->num)
+            {
+                inicio=inicio->siguiente;
+                delete aux;
+            }
+            else
+            {
+                while (aux->siguiente!=nullptr&&aux->siguiente->num!=chosen_number)
+                {
+                    aux=aux->siguiente;
+                
+                }
+                if (aux->siguiente->num==chosen_number)
+                {
+                    nodo_int* aborrar=aux->siguiente;
+                    aux->siguiente=aborrar->siguiente;
+                    delete aborrar;
+                }
+            }
+        }
+    }
+    return inicio;
+}
 
+
+
+nodo_int* elimination_of_existences(nodo_int* inicio,int &chosen_number)
+{
+    cout<<endl<< "Ingrese numero a exterminar por completo de la lista: ";
+    cin >>chosen_number;
+    inicio=exterminate_number(inicio,chosen_number);
+    return inicio;
+
+
+}
+void impresion_datos(nodo_int *inicio)
+{   
+    cout << '\t' <<"|Datos enlazados|"<<endl<<'|';
+    for (nodo_int  *aux = inicio; aux!=nullptr; aux=aux->siguiente)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        cout << aux->num<< '|';
+    }
+}
 int main()
 {
     nodo_int* inicio=nullptr;
     inicio=load_of_ints(inicio);
-
-
+    int chosen_number;
+    impresion_datos(inicio);
+    inicio=elimination_of_existences(inicio,chosen_number);
+    space_jump();
+    impresion_datos(inicio);
 
 }
